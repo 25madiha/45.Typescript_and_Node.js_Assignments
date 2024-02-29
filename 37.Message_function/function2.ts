44c5fc5ba8bc6216feea8cea15fa2a185f1ddd3f
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 02:06:35 +0500

    Add files via upload

---
+++ 37.Message_function/function2.ts
@@ -0,0 +1,6 @@
+function make_shirt(size = "L", message = "I love typescript!"):void{
+    console.log(`I'm making a ${size} T.shirt with the message "${message}". `);
+}
+make_shirt("Large");
+make_shirt("Medium");
+make_shirt("Small", "I'm a programmer");
