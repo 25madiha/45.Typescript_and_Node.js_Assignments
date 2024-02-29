22d6514c4272630f1c44346cb0be4cc519369713
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:39:27 +0500

    Add files via upload

---
+++ 28.Stages_of_life_if&ifelse/statement.ts
@@ -0,0 +1,29 @@
+let age: number = 28
+if(age < 2){
+  console.log("the person is a baby")
+}
+
+ else if (age >=2 && age <4){
+  console.log("the person is a todler")
+}
+
+else if (age >=4 && age <13){
+  console.log("the person is a kid")
+}
+
+else if (age >=13 && age <20){
+  console.log("the person is a teenager")
+}
+
+else if (age >=20 && age <65){
+  console.log("the person is adult")
+}
+
+else if (age>=65){
+  console.log("the person is a elder")
+}
+
+
+
+
+ 
