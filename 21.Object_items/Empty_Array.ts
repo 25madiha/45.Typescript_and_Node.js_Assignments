4949787bc4aaf62cbf948d3216f951d8f1f2a408
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:24:22 +0500

    Add files via upload

---
+++ 21.Object_items/Empty_Array.ts
@@ -0,0 +1,11 @@
+// think of something you could store in a array
+let items :string[] = ['Pakistan','Karachi','Urdu'];
+
+// write a program that create a list containing these items
+let item2 :string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
