8f2f69ea987a80eff71914c0a9e9bc9af6543d5e
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-02-29 23:38:24 +0500

    Add files via upload

---
+++ 14.Guest_list/Guest_list.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_list = ['madiha', 'rumi', 'azka'];
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Miss. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!');
+}
