de75af4c364f55aecbe0a1b44a5dcd1a29fe8cd0
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:30:13 +0500

    Add files via upload

---
+++ 24.Different_Task/different_task.js
@@ -0,0 +1,28 @@
+"use strict";
+// // // // //Test for equality & in equality with string
+// // // // const string1 = "Apple";
+// // // // const string2 = "orange";
+// // // // console.log(string1 === string2);
+// // // // console.log(string1 !== string2);
+// // // // test using the lowercase function
+// // // const mixedCaseString: string = "HelloWorld";
+// // // console.log(mixedCaseString.toLowerCase() === "helloworld")
+// // // numerical tests involving equality and in equality, greater than and less than,
+// // // greater than or equal to, and less than or equal to
+// // const num1:number = 10 ;
+// // const num2:number = 5;
+// // console.log(num1 === num2);
+// // console.log(num1 !== num2);
+// // console.log(num1 > num2);
+// // console.log(num1 < num2);
+// // console.log(num1 >= num2);
+// // console.log(num1 <= num2);
+// // test using "and" and or operators
+// const condition1: boolean = true;
+// const condition2: boolean = false;
+// console.log(condition1 && condition2);
+// console.log(condition1 || condition2);
+// test whether an item is in a array
+const fruits = ["apple", "orange", "banana", "grapes"];
+console.log(fruits.includes("apple"));
+console.log(fruits.includes("Apple"));
