78470cb54247139c65c4cd877ae8fc06b3d46786
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:35:07 +0500

    Add files via upload

---
+++ Alien_colors2/alien_colors.ts
@@ -0,0 +1,13 @@
+let alienColor: string = "green"
+
+   if (alienColor === "green"){
+  console.log("player just earned 5 points!")
+ }
+alienColor = "red";
+
+   if(alienColor === "green"){
+     console.log("player just earend 5 points!")
+}
+else{
+    console.log("player just earend 10 points!")
+}
