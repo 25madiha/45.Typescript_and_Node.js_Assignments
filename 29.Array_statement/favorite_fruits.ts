1bc7a151649f1865745bdfa420c5a1a67b2cc190
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:42:35 +0500

    Add files via upload

---
+++ 29.Array_statement/favorite_fruits.ts
@@ -0,0 +1,26 @@
+const favorite_fruits: string []=["banana","apple","strawberry"];
+
+if(favorite_fruits.includes("banana")){
+    console.log("you really like banana!")
+}
+
+if(favorite_fruits.includes("Apple")){
+    console.log("you really like Apple!")
+}
+
+if(favorite_fruits.includes("orange")){
+    console.log("you really like orange!")
+}
+else{
+    console.log("orange are not in your favorite_fruits list.");
+}
+if(favorite_fruits.includes("strawberry")){
+    console.log("you really like strawberry")
+}
+
+if(favorite_fruits.includes("grapes")){
+    console.log("you really like grapes!")
+}
+else{
+    console.log("grapes are not in your favorite_fruits list.")
+}
