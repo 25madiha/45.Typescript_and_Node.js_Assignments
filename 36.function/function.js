030e596add8d659939c0937b99aef05e596ae7c7
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 02:03:40 +0500

    Add files via upload

---
+++ 36.function/function.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`I'm making a ${size} T.shirt with the message "${message}"`);
+}
+make_shirt("Small", "I love typescript!");
+make_shirt("Medium", "I love typescript!");
+make_shirt("Large", "I love typescript!");
---
+++ 36.function/function.ts
@@ -0,0 +1,6 @@
+function make_shirt(size:string, message:string):void{
+    console.log(`I'm making a ${size} T.shirt with the message "${message}"`);
+}
+make_shirt("Small", "I love typescript!");
+make_shirt("Medium", "I love typescript!");
+make_shirt("Large", "I love typescript!");
