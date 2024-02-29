c8bfa39aebbf52189ee398f1cb7ee99caeb0c3fa
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:58:58 +0500

    Add files via upload

---
+++ 34.fav_Pizza_Array_loop/fav_pizza_array_loop.js
@@ -0,0 +1,6 @@
+"use strict";
+let favorite_pizza = ["extra cheez", "afghani tikka", "malai tikka"];
+for (let i = 0; i < favorite_pizza.length; i++) {
+    console.log(`i like ${favorite_pizza[i]} pizza.`);
+}
+console.log("i really love pizza");
---
+++ 34.fav_Pizza_Array_loop/fav_pizza_array_loop.ts
@@ -0,0 +1,6 @@
+let favorite_pizza: string[] = ["extra cheez","afghani tikka","malai tikka"];
+
+ for(let i = 0 ; i < favorite_pizza.length ; i++){
+    console.log(`i like ${favorite_pizza[i]} pizza.`);
+ }
+ console.log("i really love pizza")
