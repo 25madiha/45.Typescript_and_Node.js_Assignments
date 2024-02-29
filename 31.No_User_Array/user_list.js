d00b7a03f3eb2dfe62b0b53d8e2bb02a195df2df
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:46:26 +0500

    Add files via upload

---
+++ 31.No_User_Array/user_list.js
@@ -0,0 +1,18 @@
+"use strict";
+// let usernames: string[] =["moeed","ahsan","sana","hussain"];
+// if(usernames.length === 0) {
+// console.log("we nedd to find some users!")
+// }
+// else{
+// for(let i=0 ; i< usernames.length; i++);
+// }
+// console.log(usernames)
+let usernames = [];
+if (usernames.length === 0) {
+    console.log("we nedd to find some users!");
+}
+else {
+    for (let i = 0; i < usernames.length; i++)
+        ;
+}
+console.log(usernames);
---
+++ 31.No_User_Array/user_list.ts
@@ -0,0 +1,20 @@
+let usernames: string[] =["moeed","ahsan","sana","hussain"];
+
+ if(usernames.length === 0) {
+    console.log("we nedd to find some users!")
+ }
+ else{
+     for(let i=0 ; i< usernames.length; i++);
+ }
+console.log(usernames)
+// second method is to print empty user list
+
+let usernames: string[] =[];
+
+if(usernames.length === 0) {
+    console.log("we nedd to find some users!")
+}
+else{
+    for(let i=0 ; i< usernames.length; i++);
+}
+console.log(usernames)
