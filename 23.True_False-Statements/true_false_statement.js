3f0e3a8496a9674ed837ac633830b31abf6de32b
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:28:19 +0500

    Add files via upload

---
+++ 23.True_False-Statements/true_false_statement.js
@@ -0,0 +1,17 @@
+"use strict";
+let car = 'subaru';
+// console.log("is car == 'subaru' ? i predict true otherwise i pridict false")
+console.log(car == 'subaru');
+console.log(car == 'civic');
+let num = 23;
+console.log(num == 23);
+console.log(num === "23");
+let fruit = 'Apple';
+console.log(fruit == 'Apple');
+console.log(fruit == 'Mango');
+let programming_language = 'typescript';
+console.log(programming_language == 'typescript');
+console.log(programming_language == 'python');
+let fruit = 'cherry';
+console.log(fruit == 'cherry');
+console.log(fruir == 'berry');
---
+++ 23.True_False-Statements/true_false_statement.ts
@@ -0,0 +1,20 @@
+let car = 'subaru';
+// console.log("is car == 'subaru' ? i predict true otherwise i pridict false")
+console.log(car == 'subaru')
+console.log(car == 'civic')
+
+let num: any = 23
+console.log(num == 23)
+console.log(num === "23")
+
+let fruit: = 'Apple';
+console.log(fruit == 'Apple')
+console.log(fruit == 'Mango')
+
+let programming_language: = 'typescript'
+console.log(programming_language == 'typescript')
+console.log(programming_language == 'python')
+
+
+
+
