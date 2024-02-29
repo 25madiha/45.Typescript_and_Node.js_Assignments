bb21d61ffceff56586be7211b11abb019cb6df1c
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-02-29 23:15:45 +0500

    Add files via upload

---
+++ 10.Adding_Comment/program_2.js
@@ -0,0 +1,5 @@
+"use strict";
+// program written by : Madiha khan 
+// Current Date : 15-2-2024
+// this programs will add two digits.
+console.log(2 + 2);
---
+++ 10.Adding_Comment/program_2.ts
@@ -0,0 +1,6 @@
+
+// program written by : Madiha khan 
+// Current Date : 15-2-2024
+// this programs will add two digits.
+
+console.log(2+2)
