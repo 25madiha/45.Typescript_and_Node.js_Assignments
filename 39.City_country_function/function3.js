fe7eb5c1cf7ee5564cfc786f9909c1c4eb24849d
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 02:11:41 +0500

    Add files via upload

---
+++ 39.City_country_function/function3.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    console.log(`"${city}, ${country}"`);
+}
+city_country("Karachi", "Pakistan");
+city_country("New Dehli", "India");
+city_country("London", "United Kingdom");
---
+++ 39.City_country_function/function3.ts
@@ -0,0 +1,6 @@
+function city_country(city:string, country:string):void{
+    console.log(`"${city}, ${country}"`)
+}
+city_country("Karachi","Pakistan");
+city_country("New Dehli","India");
+city_country("London","United Kingdom");
