4a506ff1bb5b3cfdb00f1f1d6fd0aed1e0b6e2f5
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-02-29 23:09:47 +0500

    Delete 10.Adiing_comment directory

--- 10.Adiing_comment/program_1.ts
+++
@@ -1,5 +0,0 @@
-
-// program written by : Madiha khan
-// Current Date : 15-2-2024
-// this program will write hello world
-console.log('Hello World');
--- 10.Adiing_comment/program_2.ts
+++
@@ -1,6 +0,0 @@
-
-// program written by : Madiha khan 
-// Current Date : 15-2-2024
-// this programs will add two digits.
-
-console.log(2+2)
