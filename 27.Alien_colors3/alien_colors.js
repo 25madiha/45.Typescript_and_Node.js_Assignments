41e140a3dcc1898e5bc0436204a8873ac5a55f49
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-01 01:36:36 +0500

    Add files via upload

---
+++ 27.Alien_colors3/alien_colors.js
@@ -0,0 +1,12 @@
+"use strict";
+let alienColor = "green";
+alienColor = "yellow";
+if (alienColor === "green") {
+    console.log("player just earned 5 points!");
+}
+else if (alienColor === "yellow") {
+    console.log("player just earend 10 points!");
+}
+else if (alienColor === "red") {
+    console.log("player just earend 15 points!");
+}
---
+++ 27.Alien_colors3/alien_colors.ts
@@ -0,0 +1,14 @@
+let alienColor: string = "green"
+alienColor = "yellow"
+
+   if (alienColor === "green"){
+  console.log("player just earned 5 points!")
+ }
+
+else if(alienColor ==="yellow") {
+  console.log("player just earend 10 points!")
+}
+
+ else if(alienColor === "red"){
+ console.log("player just earend 15 points!")
+}
