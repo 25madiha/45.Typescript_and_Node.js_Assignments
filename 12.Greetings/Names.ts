f301ab4ff4e2f64d3d97080dc3356c53066fbf72
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-02-29 23:31:06 +0500

    Add files via upload

---
+++ 12.Greetings/Names.ts
@@ -0,0 +1,9 @@
+let names :string[] = ['Madiha','Moeed','Fisal','Rumi']; 
+
+const message :string = 'Welcome to typescript class : '
+
+for (let i=0; i<names.length; i++){
+
+
+console.log(message + names[i]) ;
+}
