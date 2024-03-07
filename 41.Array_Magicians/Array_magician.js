7af7042026f500a3282ef3e6b97dc5e5886f075b
Madihakhan <160606666+25madiha@users.noreply.github.com>
2024-03-07 22:14:56 +0500

    Add files via upload

---
+++ 41.Array_Magicians/Array_magician.js
@@ -0,0 +1,7 @@
+"use strict";
+function show_magicians(magicians) {
+    magicians.forEach(magician => console.log(magician));
+}
+const magicianNames = ["David copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+show_magicians(magicianNames);
+console.log(magicianNames);
---
+++ 41.Array_Magicians/Array_magician.ts
@@ -0,0 +1,8 @@
+function show_magicians(magicians:string[]):void{
+    magicians.forEach(magician => console.log(magician));
+}
+const magicianNames: string[] = ["David copperfield","Harry Houdini","Penn Jillette","Teller"];
+show_magicians(magicianNames)
+console.log(magicianNames)
+
+
